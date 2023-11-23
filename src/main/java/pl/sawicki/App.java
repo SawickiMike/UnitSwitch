2a2aa002958b3044dadddc203d73afab3745174b
SawickiMike <120393520+SawickiMike@users.noreply.github.com>
2023-11-23 10:04:57 +0100

    Creating repository
    Creating GitHub repository for project

---
+++ src/main/java/pl/sawicki/App.java
@@ -0,0 +1,13 @@
+package pl.sawicki;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
